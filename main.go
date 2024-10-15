9659118d8e7fe07a2b66fdc305dcf560954778ec
Vladislav <warzon131@gmail.com>
2024-10-16 02:13:43 +0300

    add reepl

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-)
-
 func main() {
-	paths := newPaths()
-	var userInput string
-
-	for {
-		fmt.Print("Pokedex > ")
-		fmt.Scan(&userInput)
-		if val, ok := commandsMap[userInput]; ok {
-			err := val.callback(paths)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+	startReepl()
 }
