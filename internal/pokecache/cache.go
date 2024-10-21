5dab64bc34794c1f54a134469a6d8e87319360f8
Vladislav <warzon131@gmail.com>
2024-10-22 01:29:36 +0300

    cache get

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -22,6 +22,13 @@ func (c *Cache) Add(url string, data []byte) {
 	}()
 }
 
+func (c *Cache) Get(key string) ([]byte, bool) {
+	if cache, ok := c.entry[key]; ok {
+		return cache.val, true
+	}
+	return []byte{}, false
+}
+
 func NewCache() *Cache {
 	return &Cache{
 		mu:    sync.Mutex{},
